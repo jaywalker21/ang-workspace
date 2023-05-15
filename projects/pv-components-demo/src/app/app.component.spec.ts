862ba6000ee766c9136f0a49bfa8d2fd7cffef59
Jay Bhatt <jay.bhatt@razorpay.com>
2023-05-15 16:35:37 +0530

    chore: change demo app name

--- projects/pv-components-demo/src/app/app.component.spec.ts
+++ projects/pv-components-demo/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pv-components-demo'`, () => {
+  it(`should have as title 'pv-app-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pv-components-demo');
+    expect(app.title).toEqual('pv-app-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pv-components-demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pv-app-demo app is running!');
   });
 });
